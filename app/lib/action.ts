12e074fd5099d930807663cb3a8dfabb7ce6a0fc
MacStudio_HOME <ssh521@naver.com>
2025-12-15 22:13:48 +0900

    12강 까지 진행

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -6,6 +6,7 @@ import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { randomUUID } from 'crypto';
 
+
 async function getConnection() {
   return mysql.createConnection({
     host: process.env.MYSQL_HOST || 'localhost',
@@ -47,6 +48,46 @@ export async function createInvoice(formData: FormData) {
     await db.end();
   }
 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function updateInvoice(id: string, formData: FormData) {
+
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
+  
+  const date = new Date().toISOString().split('T')[0];
+
+  const db = await getConnection();
+  try {
+    await db.execute(
+      `UPDATE invoices SET customer_id = ?, amount = ?, status = ?, date = ? WHERE id = ?`,
+      [customerId, amountInCents, status, date, id],
+    );
+  } finally {
+    await db.end();
+  }
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+  
+  const db = await getConnection();
+  try {
+    await db.execute(`DELETE FROM invoices WHERE id = ?`, [id]);
+  } finally {
+    await db.end();
+  }
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
